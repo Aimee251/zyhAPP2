c196c2f1caf26bef725daa08514b299cd5718ece
Aimee Zhou <ztianyu951@gmail.com>
2025-05-24 23:24:22 +0800

    Initial Commit

---
+++ zyhAPP6/zyhAPP6App.swift
@@ -0,0 +1,17 @@
+//
+//  zyhAPP6App.swift
+//  zyhAPP6
+//
+//  Created by tianyu zhou on 2025-05-24.
+//
+
+import SwiftUI
+
+@main
+struct zyhAPP6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ zyhAPP6Tests/zyhAPP6Tests.swift
@@ -0,0 +1,17 @@
+//
+//  zyhAPP6Tests.swift
+//  zyhAPP6Tests
+//
+//  Created by tianyu zhou on 2025-05-24.
+//
+
+import Testing
+@testable import zyhAPP6
+
+struct zyhAPP6Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
